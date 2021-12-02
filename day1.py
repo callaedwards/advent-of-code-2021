35f298ee3020a09337dcf3f5b07fdb53c6882d1c
calla <callaed98@gmail.com>
2021-12-02 13:57:45 -0500

    clean up

--- day1.py
+++ day1.py
@@ -1,12 +1,33 @@
-
-count = 0
-
-with open('input.txt') as f:
-	lines = f.readlines()
-	curr = int(lines[0])
+def count_value_increases(lines: [str]) -> int:
+	count = 0
+	prev_value = int(lines[0])
 	for line in lines[1:]:
-		if curr < int(line):
+		if prev_value < int(line):
 			count += 1
-		curr = int(line)
+		prev_value = int(line)
+	return count
 
-print(count)
+def count_window_increases(lines: [str]) -> int:
+	count = 0
+	v1 = int(lines[1])
+	v2 = int(lines[2])
+	prev_sum = int(lines[0]) + v1 + v2
+
+	for line in lines[3:]:
+		curr_sum = v1 + v2 + int(line)
+		if prev_sum < curr_sum:
+			count += 1
+		prev_sum = curr_sum
+		v1 = v2
+		v2 = int(line)
+	return count
+
+def main():
+	with open('input.txt') as f:
+		lines = f.readlines()
+		c1 = count_value_increases(lines)
+		c2 = count_window_increases(lines)
+	print("part1:", c1,"\npart2:", c2)
+
+if __name__ == "__main__":
+    main()
