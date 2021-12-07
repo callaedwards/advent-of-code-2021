c49b2131385a38eb023205fe0a2fffb25e85d33c
calla <callaed98@gmail.com>
2021-12-07 16:52:07 -0500

    day 3

--- day3.py
+++ day3.py
@@ -2,45 +2,108 @@ from setup import get_input_file_path
 from typing import List
 
 
-def find_most_common_bits(binaries: List[str]) -> str:
-	ans = [0]*len(binaries[0])
-	for binary in binaries:
+
+def find_most_common_bits(binary_readings: List[str]) -> str:
+	column_sums = [0]*len(binary_readings[0])
+	for binary in binary_readings:
 		for i in range(len(binary)):
-			ans[i] += int(binary[i])
+			column_sums[i] += int(binary[i])
 
-	threshold = len(binaries) // 2
-	b = ""
-	for a in ans:
-		if a > threshold:
-			b += "1"
-		else:
-			b += "0"
-	return b
+	threshold = len(binary_readings) // 2
 
-def find_least_common_bits(binary: str) -> str:
-	c = ""
-	for b in binary:
-		if b == "1":
-			c += "0"
+	binary_string = ""
+	for col in column_sums:
+		if col > threshold:
+			binary_string += "1"
 		else:
-			c += "1"
-	return c
+			binary_string += "0"
+	return binary_string
+
+def invert_bit_string(input_binary_string: str) -> str:
+	output_binary_string = ""
+	for bit in input_binary_string:
+		if bit == "1":
+			output_binary_string += "0"
+		else:
+			output_binary_string += "1"
+	return output_binary_string
 
 def binary_to_decimal(binary: str) -> int:
 	return int(binary, 2)
+
+def part1(binary_readings: List[str]) -> str:
+	most_common_bits = find_most_common_bits(binary_readings)
+	least_common_bits = invert_bit_string(most_common_bits)
+
+	gamma_rate = binary_to_decimal(most_common_bits)
+	epsilon_rate = binary_to_decimal(least_common_bits)
+
+	power_consumption = gamma_rate * epsilon_rate
+	return(power_consumption)
+
+
+def find_most_common_bit(binary_readings: List[str], index: int) -> str:
+	sum = 0
+	for binary in binary_readings:
+		sum += int(binary[index])
+
+	threshold = len(binary_readings) / 2
+	if sum >= threshold:
+		return 1
+	else:
+		return 0
+
+
+def find_least_common_bit(binary_readings: List[str], index: int) -> str:
+	sum = 0
+	for binary in binary_readings:
+		sum += int(binary[index])
+
+	threshold = len(binary_readings) / 2
+
+	if sum >= threshold:
+		return 0
+	else:
+		return 1
+
+
+def find_match(binary_readings: List[str], most_common: bool):
+	match = ""
+	matches = binary_readings
+	i = 0
+	while(len(matches) > 1):
+		if most_common:
+			desired_bit = find_most_common_bit(matches, i)
+		else:
+			desired_bit = find_least_common_bit(matches, i)
+		match += str(desired_bit)
+		matches = [b for b in binary_readings if b.startswith(match)]
+		i += 1
+	return matches[0]
+
+
+def part2(binary_readings: List[str]) -> str:
+	most_common_bit_result = find_match(binary_readings, True)
+	oxygen_generator_reating = binary_to_decimal(most_common_bit_result)
+
+	least_common_bit_result = find_match(binary_readings, False)
+	co2_scrubber_rating = binary_to_decimal(least_common_bit_result)
+
+	life_support_rating = oxygen_generator_reating * co2_scrubber_rating
+
+	return(life_support_rating)
 
 
 def main():
 	input_file = get_input_file_path()
 	with open(input_file) as f:
-		binaries = [line.strip() for line in f.readlines()]
-		f1 = find_most_common_bits(binaries)
-		f2 = find_least_common_bits(f1)
+		binary_readings = [line.strip() for line in f.readlines()]
+		a1 = part1(binary_readings)
+		a2 = part2(binary_readings)
 
-	print(f1,f2)
-	print(binary_to_decimal(f1) * binary_to_decimal(f2))
-		#c2 = count_depth_window_increases(depths)
-	#print("part1:", c1,"\npart2:", c2)
+	print(a1)
+	print(a2)
+
 
 if __name__ == "__main__":
     main()
