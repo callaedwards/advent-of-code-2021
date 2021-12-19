9c6ce00307b4a4b6ad7a198929e498084e3b43fe
calla <callaed98@gmail.com>
2021-12-19 18:11:51 -0500

    day4 pt 1

--- day4.py
+++ day4.py
@@ -3,8 +3,8 @@ from typing import List, Tuple
 
 BOARD_SIZE = 5
 
-class Board:
 
+class Board:
 	def __init__(self, board: List[List[str]]):
 		rows = []
 		columns = []
@@ -26,76 +26,73 @@ class Board:
 			self.rows[index].remove(target_value)
 		else:
 			self.columns[index].remove(target_value)
-		#axis[index].add(target_value * -1)
 
 
-def part1(bingo_boards: List[List[List[str]]], numbers_called: List[str]) -> int:
-	boards = []
-	for bingo_board in bingo_boards:
-		boards += [Board(bingo_board)]
+def part1(bingo_boards: List[List[List[str]]], numbers_called: List[int]) -> int:
+	boards = create_boards(bingo_boards)
 
 	winning_board = None
-	winning_move_count = len(numbers_called)
+	winning_move_index = len(numbers_called)
+	
 	for board in boards:
-		move_count = check_win(board, numbers_called)
-		if move_count < winning_move_count:
-			winning_move_count = move_count
+		print(board)
+		move_index = find_win_index(board, numbers_called)
+		if move_index < winning_move_index:
+			winning_move_index = move_index
 			winning_board = board
 
 	total = 0
-	for row in board.rows:
+	for row in winning_board.rows:
 		while len(row) > 0:
 			total += row.pop()
 
-	#print(numbers_called)
-	#print(len(numbers_called))
-	#print(winning_move_count)
-	multiplier = int(numbers_called[winning_move_count])
-	print(total)
-	print(multiplier)
+	multiplier = numbers_called[winning_move_index]
 	return total * multiplier
 
 
-def check_win(board: Board, numbers_called: List[str]) -> int:
-	i = 1
-	is_done = False
-	while i < len(numbers_called) and not is_done:
+def create_boards(bingo_boards: List[List[List[str]]]) -> List[Board]:
+	return [Board(bingo_board) for bingo_board in bingo_boards]
+
+
+def find_win_index(board: Board, numbers_called: List[int]) -> int:
+	for i in range(len(numbers_called)):
+		target_value = numbers_called[i]
+
 		# check_horizontals()
 		for r in range(len(board.rows)):
-			target_value = int(numbers_called[i])
 			if target_value in board.rows[r]:
 				board.mark(r, target_value, True)
+				break
 
 		# check_verticals()
 		for c in range(len(board.columns)):
-			target_value = int(numbers_called[i])
-			if target_value in board.rows[c]:
+			if target_value in board.columns[c]:
 				board.mark(c, target_value, False)
+				break
 		
-		for j in range(len(board.rows)):
-			if len(board.rows[r]) == 0 or len(board.columns[c]) == 0:
-				is_done = True
+		for row in board.rows:
+			if len(row) == 0:
+				return i
 
-		i += 1
-	return i
+		for column in board.columns:
+			if len(column) == 0:
+				return i
 
-# def check_horizontals():
-# 	pass
-
+	return i + 1
 
 
 def main():
-	#input_file = get_input_file_path()
-	input_file = "inputs/day4test.txt"
+	input_file = get_input_file_path()
 	with open(input_file) as f:
 		bingo_input = f.read().split("\n\n")
 		numbers_called = bingo_input[0].split(",")
+		parsed_numbers_called = [int(num) for num in numbers_called]
 
 		parsed_boards = [board.split() for board in bingo_input[1:]]
 		for i in range(len(parsed_boards)):
 			parsed_boards[i] = [parsed_boards[i][b:b+BOARD_SIZE] for b in range(0, len(parsed_boards[i]), BOARD_SIZE)]
 
-		a1 = part1(parsed_boards, numbers_called)
+		a1 = part1(parsed_boards, parsed_numbers_called)
 		# a2 = part2(binary_readings)
 	
 	print(a1)
@@ -103,7 +100,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+	main()
+
 
 
 
