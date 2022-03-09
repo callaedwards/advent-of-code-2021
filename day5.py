e0523aee690a20aa1c1100de1dc44a5f942b9742
calla <callaed98@gmail.com>
2022-03-09 17:00:00 -0500

    day5

--- day5.py
+++ day5.py
@@ -1,32 +1,51 @@
 from setup import get_input_file_path
-from typing import List
+from typing import List, Dict, Tuple
 
-coordinate_plane = {}
+sample_data = [
+                "0,9 -> 5,9",
+                "8,0 -> 0,8",
+                "9,4 -> 3,4",
+                "2,2 -> 2,1",
+                "7,0 -> 7,4",
+                "6,4 -> 2,0",
+                "0,9 -> 2,9",
+                "3,4 -> 1,4",
+                "0,0 -> 8,8",
+                "5,5 -> 8,2"
+            ]
 
 
 class Line:
     def __init__(self, line: List[str]):
-
         start, end = line
-        self.start = Point(start)
-        self.end = Point(end)
+        self.start = Point.from_string(start)
+        self.end = Point.from_string(end)
 
     def __str__(self) -> str:
         return "{" + str(self.start) + ", " + str(self.end) + "}"
 
 
 class Point:
-    def __init__(self, coordinates: str):
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def from_string(cls, coordinates: str):
         x, y = coordinates.split(",")
-        self.x = int(x)
-        self.y = int(y)
+        x = int(x)
+        y = int(y)
+        return cls(int(x), int(y))
 
     def __str__(self) -> str:
         return "(" + str(self.x) + ", " + str(self.y) + ")"
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
 
 def create_lines(lines: List[List[str]]) -> List[Line]:
-    return [Line(line) for line in lines if is_valid(line)]
+    return [Line(line) for line in lines]
 
 
 def is_valid(line: Line) -> bool:
@@ -35,25 +54,55 @@ def is_valid(line: Line) -> bool:
     return False
 
 
-def count_intersections(line1: Line, line2: Line) -> int:
-    pass
-    #if line1.start.x == line2.end.x:
+def find_intersections(lines: List[Line], include_diagonals=False):
+    point_dict = {}
+    for line in lines:
+        points = get_points(line, include_diagonals)
+        for point in points:
+            tupled_point = (point.x, point.y)
+            if tupled_point in point_dict:
+                point_dict[tupled_point] += 1
+            else:
+                point_dict[tupled_point] = 1
 
-    # else line.start.y == line.end.y:
-
-    # 1. [3, 6], [5, 6]
-    # 2. [3, 2], [3, 8]
-    # 3. [4, 6], [6, 6]
-    # 1 & 2 -> 1: [3,6]
-    # 1 & 3 -> 2: [4, 6], [5, 6]
-
-
-def find_intersections(lines: List[Line]):
-    for j in range(len(lines)):
-        for k in range(j+1, len(lines)):
-            count_intersections(lines[j], lines[k])
+    return count_intersections(point_dict)
 
 
+def get_points(line: Line, include_diagonals: bool) -> List[Point]:
+    points = []
+    if line.start.x == line.end.x:
+        lower_y_bound = min(line.start.y, line.end.y)
+        upper_y_bound = max(line.start.y, line.end.y)
+        for y in range(lower_y_bound, upper_y_bound + 1):
+            points.append(Point(line.start.x, y))
+
+    elif line.start.y == line.end.y:
+        lower_x_bound = min(line.start.x, line.end.x)
+        upper_x_bound = max(line.start.x, line.end.x)
+        for x in range(lower_x_bound, upper_x_bound + 1):
+            points.append(Point(x, line.start.y))
+
+    elif include_diagonals and abs(line.start.x-line.end.x) == abs(line.start.y-line.end.y):
+        x_inc = 1 if line.start.x < line.end.x else -1
+        y_inc = 1 if line.start.y < line.end.y else -1
+
+        x = line.start.x
+        y = line.start.y
+        while x != line.end.x and y != line.end.y:
+            points.append(Point(x, y))
+            x += x_inc
+            y += y_inc
+        points.append(Point(x, y))
+    return points
+
+
+def count_intersections(points: Dict[Tuple[int, int], int]) -> int:
+    count = 0
+    for point in points:
+        if points[point] >= 2:
+            count += 1
+
+    return count
 
 
 def main():
@@ -63,10 +112,10 @@ def main():
 
         parsed_lines = [line.strip().split(" -> ") for line in lines_input]
         lines = create_lines(parsed_lines)
-        for line in lines:
-            print(line)
 
-        find_intersections(lines)
+        c1 = find_intersections(lines)
+        c2 = find_intersections(lines, True)
+        print("part1:", c1, "\npart2:", c2)
 
 
 if __name__ == "__main__":
